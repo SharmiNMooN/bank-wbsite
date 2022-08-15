58f541f54b69ab66a6fac7fb69e8928eaf5b2be1
Sharmin Moon <sharminmoon0308@gmail.com>
2022-08-15 23:21:14 +0600

    get email submission done

--- js/login.js
+++ js/login.js
@@ -1,5 +1,15 @@
 // step-1: add a click event handler with submit buttongit
 
 document.getElementById('submit-btn').addEventListener('click', function(){
-    console.log('Submit Button Clicked');
-})
+//     console.log('Submit Button Clicked');
+// step-2: get the email address inside email field
+//always remember to use .value to get text from an input field
+
+const emailField = document.getElementById('user-email');
+const email = emailField.value;
+console.log(email);
+
+
+
+
+ })

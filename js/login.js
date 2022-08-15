cb35faeae1a6a3f69706b6a621541bcd223c3ecf
Sharmin Moon <sharminmoon0308@gmail.com>
2022-08-15 23:08:37 +0600

    new js file added and click event edded

---
+++ js/login.js
@@ -0,0 +1,5 @@
+// step-1: add a click event handler with submit buttongit
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+    console.log('Submit Button Clicked');
+})

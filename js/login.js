fd6721356645a73a499aa9f204a57b4bc4bea6bd
Sharmin Moon <sharminmoon0308@gmail.com>
2022-08-16 20:19:40 +0600

    new html file added and first section is done

--- js/login.js
+++ js/login.js
@@ -1,30 +1,29 @@
 // step-1: add a click event handler with submit buttongit
 
-document.getElementById('submit-btn').addEventListener('click', function(){
-//     console.log('Submit Button Clicked');
-// step-2: get the email address inside email field
-//always remember to use .value to get text from an input field
+document.getElementById("submit-btn").addEventListener("click", function () {
+  //     console.log('Submit Button Clicked');
+  // step-2: get the email address inside email field
+  //always remember to use .value to get text from an input field
 
-const emailField = document.getElementById('user-email');
-const email = emailField.value;
-// console.log(email);
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+  // console.log(email);
 
-// step-3: get a password
-// 3.a. set id on the html element
-// 3.b. get the element
-// 3.c. get the value from the element
-const passwordField = document.getElementById('user-password');
-const password = passwordField.value;
-// console.log(email, password);
+  // step-3: get a password
+  // 3.a. set id on the html element
+  // 3.b. get the element
+  // 3.c. get the value from the element
+  const passwordField = document.getElementById("user-password");
+  const password = passwordField.value;
+  // console.log(email, password);
 
-// denger: do not verify email and password on the  clint side 
-// step-4: verify email and password
-if(email === 'sm@islamibank.com' && password === 'sm0003'){
-    console.log('valid user');
-}else{
-    console.log('invalid user');
-}
-
-
-
- })
+  // denger: do not verify email and password on the  clint side
+  // step-4: verify email and password
+  if (email === "sm@islamibank.com" && password === "sm0003") {
+    // console.log('valid user');
+    window.location.href = "bank.html";
+  } else {
+    // console.log('invalid user');
+    alert("invalid user");
+  }
+});

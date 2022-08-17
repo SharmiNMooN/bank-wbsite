7db459726a459278f276a84535622bfe316cc2c2
Sharmin Moon <sharminmoon0308@gmail.com>
2022-08-17 18:03:24 +0600

    added current deposit amont

--- js/deposit.js
+++ js/deposit.js
@@ -5,14 +5,24 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   // for input field use.value to the value inside the input field
 
   const depositField = document.getElementById("deposit-field");
-  const depositAmount = depositField.value;
+  const newDepositAmountString = depositField.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
+  console.log(typeof newDepositAmount);
 
   // step-3 get the current deposit total
   // for non-input(element other than input, textarea) use inner text to get the text
 
   const depositTotalElement = document.getElementById("deposit-total");
-  const depositTotal = depositTotalElement.innerText;
+  const previousDepositTotalString = depositTotalElement.innerText;
+  const previousDepositTotal = parseFloat(previousDepositTotalString);
+  //   console.log(typeof previousDepositTotal);
+  const currentDepositeTotal = previousDepositTotal + newDepositAmount;
+
   //   console.log(depositTotal);
-  depositTotalElement.innerText = depositAmount;
-  console.log(depositAmount);
+  depositTotalElement.innerText = currentDepositeTotal;
+  //   console.log(depositAmount);
 });
+
+// step-7: clear the deposite feild
+
+depositField.value = "";

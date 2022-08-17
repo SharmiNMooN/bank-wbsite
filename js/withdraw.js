b896add0c8d2b7b653fbe79ce3f9ad257148984a
Sharmin Moon <sharminmoon0308@gmail.com>
2022-08-17 23:13:29 +0600

    previous balance total added

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,6 +4,10 @@
 2.5. Also make sure convert the input field into a number by using parsefloat
 3.get previous withdraw total
 4.calculate total withdraw amont
+4.5. get total withdraw amount
+5. get previous balance total
+
+7.clear the input field
 
 
 
@@ -32,6 +36,11 @@ document.getElementById("btn-withdraw").addEventListener("click", function () {
   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
   withdrawTotalElement.innerText = currentWithdrawTotal;
 
+  //step-5:
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  console.log(previousBalanceTotal);
   //step-7: clear the input field
   withdrawField.value = "";
 });
